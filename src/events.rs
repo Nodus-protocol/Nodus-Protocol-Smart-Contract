2e488fc3ca09df97fe3b54325d528d4929d9605a
Jaydbrown <jaiyeolawety705@gmail.com>
2026-05-31 01:12:47 +0100

    feat: migrate src/events.rs to Soroban

--- src/events.rs
+++ src/events.rs
@@ -1,37 +1,58 @@
-use ink::primitives::AccountId;
+#![no_std]
+use soroban_sdk::{contracttype, symbol_short, Address, Env};
 
-#[ink::event]
-pub struct Mint {
-    #[ink(topic)]
-    pub sender: AccountId,
-    pub amount_0: u128,
-    pub amount_1: u128,
+#[contracttype]
+pub struct MintEvent {
+    pub sender: Address,
+    pub amount_0: i128,
+    pub amount_1: i128,
 }
 
-#[ink::event]
-pub struct Burn {
-    #[ink(topic)]
-    pub sender: AccountId,
-    pub amount_0: u128,
-    pub amount_1: u128,
-    #[ink(topic)]
-    pub to: AccountId,
+#[contracttype]
+pub struct BurnEvent {
+    pub sender: Address,
+    pub amount_0: i128,
+    pub amount_1: i128,
+    pub to: Address,
 }
 
-#[ink::event]
-pub struct Swap {
-    #[ink(topic)]
-    pub sender: AccountId,
-    pub amount_0_in: u128,
-    pub amount_1_in: u128,
-    pub amount_0_out: u128,
-    pub amount_1_out: u128,
-    #[ink(topic)]
-    pub to: AccountId,
+#[contracttype]
+pub struct SwapEvent {
+    pub sender: Address,
+    pub amount_0_in: i128,
+    pub amount_1_in: i128,
+    pub amount_0_out: i128,
+    pub amount_1_out: i128,
+    pub to: Address,
 }
 
-#[ink::event]
-pub struct Sync {
-    pub reserve_0: u128,
-    pub reserve_1: u128,
+#[contracttype]
+pub struct SyncEvent {
+    pub reserve_0: i128,
+    pub reserve_1: i128,
+}
+
+pub fn emit_mint(env: &Env, sender: Address, amount_0: i128, amount_1: i128) {
+    env.events().publish((symbol_short!("mint"),), MintEvent { sender, amount_0, amount_1 });
+}
+
+pub fn emit_burn(env: &Env, sender: Address, amount_0: i128, amount_1: i128, to: Address) {
+    env.events().publish((symbol_short!("burn"),), BurnEvent { sender, amount_0, amount_1, to });
+}
+
+pub fn emit_swap(
+    env: &Env,
+    sender: Address,
+    amount_0_in: i128, amount_1_in: i128,
+    amount_0_out: i128, amount_1_out: i128,
+    to: Address,
+) {
+    env.events().publish(
+        (symbol_short!("swap"),),
+        SwapEvent { sender, amount_0_in, amount_1_in, amount_0_out, amount_1_out, to },
+    );
+}
+
+pub fn emit_sync(env: &Env, reserve_0: i128, reserve_1: i128) {
+    env.events().publish((symbol_short!("sync"),), SyncEvent { reserve_0, reserve_1 });
 }
